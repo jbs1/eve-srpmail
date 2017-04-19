cfecece3fd3d92e2de85be71666b683754827c5b
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-19 02:40:30 +0200

    bla

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$api_instance = new Swagger\Client\Api\AllianceApi();
+$datasource = "tranquility"; // string | The server name you would like data from
+$user_agent = "user_agent_example"; // string | Client identifier, takes precedence over headers
+$x_user_agent = "x_user_agent_example"; // string | Client identifier, takes precedence over User-Agent
+
+try {
+    $result = $api_instance->getAlliances($datasource, $user_agent, $x_user_agent);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling AllianceApi->getAlliances: ', $e->getMessage(), PHP_EOL;
+}
+
+?>
