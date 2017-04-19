65340dcf6a11a5f071f7b74d542ef0894e81392d
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-19 22:27:19 +0200

    oauth test 2

--- oauth.php
+++ oauth.php
@@ -9,7 +9,7 @@ require_once('vendor/autoload.php');
 $provider = new \League\OAuth2\Client\Provider\GenericProvider([
     'clientId'                => '1dec8b1b5aca4667924f373756d34b75',    // The client ID assigned to you by the provider
     'clientSecret'            => 'ahTmVKy0Zp93j3WDfrhrFVqcy1wChPiITwaUaDwr',   // The client password assigned to you by the provider
-    'redirectUri'             => 'http://ec2-54-224-182-102.compute-1.amazonaws.com/eve/callback.php',
+    'redirectUri'             => 'http://ec2-54-224-182-102.compute-1.amazonaws.com/eve/oauth.php',
     'urlAuthorize'            => 'https://login.eveonline.com/oauth/authorize',
     'urlAccessToken'          => 'https://login.eveonline.com/oauth/token',
     'urlResourceOwnerDetails' => 'https://login.eveonline.com/oauth/verify'
