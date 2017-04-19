db2cbdb271a6c4584691712557bc13a3d88c1d22
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-19 21:15:31 +0200

    bla

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $api_instance = new Swagger\Client\Api\AllianceApi();
 $datasource = "tranquility"; // string | The server name you would like data from
 $user_agent = "user_agent_example"; // string | Client identifier, takes precedence over headers
 $x_user_agent = "x_user_agent_example"; // string | Client identifier, takes precedence over User-Agent
-echo "bla";
+
 try {
     $result = $api_instance->getAlliances($datasource, $user_agent, $x_user_agent);
     print_r($result);
