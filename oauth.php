e2b3fca20f8d82a2a2184180645e99f58b765acf
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-20 03:38:44 +0200

    oauth

--- oauth.php
+++ oauth.php
@@ -21,7 +21,7 @@ if (!isset($_GET['code'])) {
     // Fetch the authorization URL from the provider; this returns the
     // urlAuthorize option and generates and applies any necessary parameters
     // (e.g. state).
-    $authorizationUrl = $provider->getAuthorizationUrl();
+    $authorizationUrl = $provider->getAuthorizationUrl(['scope'=>['characterContractsRead']]);
 
     // Get the state generated for you and store it to the session.
     $_SESSION['oauth2state'] = $provider->getState();
