f8185195f8d874f7cb57a565e771c3402452aa1f
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-21 16:40:22 +0200

    contract testing

--- inc.php
+++ inc.php
@@ -37,15 +37,15 @@ function getcontract($charid,$token){
 	// $url="https://raw.githubusercontent.com/jbs1/eve-srpmail/master/Contracts.xml.xml";
 	print $url;
 	// $answer=file_get_contents($url);
-	$ch=curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-    	curl_setopt($ch, CURLOPT_FAILONERROR,1);
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-	$raw=curl_exec($ch);
-	curl_close($ch);
+	// $ch=curl_init();
+	// curl_setopt($ch, CURLOPT_URL, $url);
+ //    	curl_setopt($ch, CURLOPT_FAILONERROR,1);
+ //    	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+ //    	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	// $raw=curl_exec($ch);
+	// curl_close($ch);
 
-	$xml = simplexml_load_string($raw);
+	$xml = simplexml_load_file($url);
 
 	return $xml;
 }
