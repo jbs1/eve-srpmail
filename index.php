28c738034d7821ab0d4167c211a8f90bbfe47c5e
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-21 23:22:03 +0200

    styling

--- index.php
+++ index.php
@@ -13,21 +13,37 @@ if(empty($_SESSION['accesstoken-obj'])){//if not logged in redirect to
 }
 
 
-echo "<!DOCTYPE html>\n<html>\n<head>\n";
+echo '<!DOCTYPE html>
+<html>
+<head>';
 
-echo'
+echo'<!-- JQuery -->
+<script
+  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+  crossorigin="anonymous"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-';
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
 
-echo "<title>srp mail</title>\n</head>\n<body>\n";
+echo"
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+";
 
 
-echo "</body>\n</html>\n";
+echo '<title>srp mail</title>
+</head>
+<body>';
+
+
+echo '</body>
+</html>';
 
 
 
