bfb920102464764d6109350aa7ed7067f6a02fea
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-21 23:38:36 +0200

    styling

--- index.php
+++ index.php
@@ -40,12 +40,6 @@ echo '<!-- JQuery -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 ';
 
-echo"<script>
-$('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})</script>";
-
 
 echo '
 <div class="container-fluid">
@@ -58,15 +52,13 @@ echo '
 				<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
 				<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
 				<li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-				<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
 			</ul>
 
 			<!-- Tab panes -->
 			<div class="tab-content">
-				<div role="tabpanel" class="tab-pane active" id="home">...</div>
-				<div role="tabpanel" class="tab-pane" id="profile">...</div>
-				<div role="tabpanel" class="tab-pane" id="messages">...</div>
-				<div role="tabpanel" class="tab-pane" id="settings">...</div>
+				<div role="tabpanel" class="tab-pane active" id="home">bla</div>
+				<div role="tabpanel" class="tab-pane" id="profile">tab2</div>
+				<div role="tabpanel" class="tab-pane" id="messages">tab3</div>
 			</div>
 		</div>
 		<div class="col-md-2"></div>
