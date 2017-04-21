db33f9bcb72daf06ef24c063199aaf3f546cb232
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-21 23:02:14 +0200

    bootstrap

--- index.php
+++ index.php
@@ -13,9 +13,22 @@ if(empty($_SESSION['accesstoken-obj'])){//if not logged in redirect to
 }
 
 
-echo "<pre>";
-print_r(getcontract(charid(),token()));
-echo "</pre>"
+echo '<html>\n<head>\n';
+
+echo'
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+';
+
+echo '<\head>\n<body>\n';
+
+
+echo '<\body>\n<\html>\n';
+
 
 
 // $api_instance = new Swagger\Client\Api\MailApi();
