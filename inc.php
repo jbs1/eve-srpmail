cb7fc0c2c76ef9625e8ee8331228d254b9a33aac
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-21 16:56:27 +0200

    contract testing

--- inc.php
+++ inc.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
-require('SwaggerClient-php/autoload.php');
-require('vendor/autoload.php');
-require('provider.php');
+require_once('SwaggerClient-php/autoload.php');
+require_once('vendor/autoload.php');
+require_once('provider.php');
 
 
 
 function token_refresh()//refresh access token if expired
 {
-
+	global $provider;
 	if(unserialize($_SESSION['accesstoken-obj'])->hasExpired()){//get new access token if expired
 		$newAccessToken=$provider->getAccessToken('refresh_token', [
 			'refresh_token' => unserialize($_SESSION['accesstoken-obj'])->getRefreshToken()
