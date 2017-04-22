fe1b54dffbdcef8a5b571fa8d098c59e9e76bfef
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-22 13:36:08 +0200

    ajax endpoints

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -8,10 +8,11 @@ require_once('../inc.php');
 header('Content-Type: application/json;charset=utf-8');
 $raw=getcontract(charid(),token());
 $json=array();
+print_r($raw['result']['row'] );
 $json['@attributes']=$raw['result']['@attributes'];
-foreach ($raw['result']['row'] as $key => $value) {
-	$json['row'][$key]=$value['@attributes'];
-}
+// foreach ($raw['result']['row'] as $key => $value) {
+// 	$json['row'][$key]=$value['@attributes'];
+// }
 print_r($json);
 print_r(getcontract(charid(),token()));
 echo json_encode(getcontract(charid(),token()));
