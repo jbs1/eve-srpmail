50dc5196106e279f089373aadc1f1edf456eb385
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-22 14:18:03 +0200

    ajax endpoints

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -10,6 +10,7 @@ token_refresh();
 header('Content-Type: application/json;charset=utf-8');
 $raw=getcontract(charid(),token());
 $json=array();
+print_r($raw->result->rowset->attributes());
 $json['attributes']=$raw->result->rowset->attributes();
 $json['row']=array();
 // print_r($raw->result->rowset);
@@ -17,7 +18,7 @@ foreach ($raw->result->rowset->row as $value) {
 	array_push($json['row'],$value->attributes());
 }
 
-print_r($json);
+// print_r($json);
 // print_r($raw);
 // echo json_encode(getcontract(charid(),token()));
 
