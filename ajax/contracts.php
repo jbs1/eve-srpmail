da7286d87b70b2a6859198dcc77cc39dfe273919
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-22 15:02:34 +0200

    ajax endpoints

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -9,29 +9,8 @@ token_refresh();
 
 header('Content-Type: application/json;charset=utf-8');
 $raw=getcontract(charid(),token());
-$json=new stdClass;
-$json=$raw->result->rowset->attributes();
-// print_r($json);
-
-// foreach ($raw->result->rowset->attributes() as $key => $value) {
-// 	$json->attributes->$key=$value[0];
-// }
-// print_r($json);
-// // // $raw->result->rowset->attributes()
-// // $json['attributes']=$raw->result->rowset->attributes();
-// // $json['row']=array();
-// // print_r($raw->result->rowset);
-$json->row="wsadsa";
-// print_r($raw->result->rowset->row);
-// foreach ($raw->result->rowset->row as $value) {
-// 	array_push($json->row,$value->attributes());
-// 	// foreach ($value->attributes() as $key => $value) {
-// 	// 	$json['row'][$key]=$value[0];
-// 	// }
-// }
-print_r($json);
-// print_r($json);
-// print_r($raw);
-// echo json_encode(getcontract(charid(),token()));
+$json=$raw->result->rowset;
+print_r($json)
+echo json_encode(getcontract(charid(),token()));
 
 ?>
