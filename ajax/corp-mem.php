38c03c5e7d1f476106307f50e85c95c59fdf3051
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-25 15:27:37 +0200

    Update corp-mem.php

--- ajax/corp-mem.php
+++ ajax/corp-mem.php
@@ -9,16 +9,26 @@ token_refresh();
 
 header('Content-Type: application/json;charset=utf-8');
 
-$api_instance = new Swagger\Client\Api\CorporationApi();
+$api_crop = new Swagger\Client\Api\CorporationApi();
+$api_universe = new Swagger\Client\Api\UniverseApi();
 $datasource = "tranquility"; // string | The server name you would like data from
 
-
 try {
-    $corpmem = $api_instance->getCorporationsCorporationIdMembers(corpid(charid()),$datasource,token());
-    $json = array();
+    $corpmem = $api_corp->getCorporationsCorporationIdMembers(corpid(charid()),$datasource,token());
+    $ids = array();
     foreach ($corpmem as $key => $value) {
-    	$json[$key]=$value['character_id'];
+    	$ids[$key]=$value['character_id'];
     }
+    $split_ids=array_chunk($ids,1000);
+    $json=array();
+    foreach ($split_ids as $value){
+        $json=array_merge($json,$api_universe->postUniverseNames($value, $datasource));
+    }
+    echo'
+    <script>
+    console.log(\'json\',JSON.parse('.json_encode($json).'));
+    </script>
+    ';
     echo json_encode($json);
 } catch (Exception $e) {
     echo 'Exception: ', $e->getMessage(), PHP_EOL;
