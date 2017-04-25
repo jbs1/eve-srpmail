884bb3e427cb6be770ffd6f3de5864d039c226b7
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-25 18:32:00 +0200

    contract refin

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -10,9 +10,9 @@ token_refresh();
 header('Content-Type: application/json;charset=utf-8');
 $raw=getcontract(charid(),token())->result->rowset;
 $json=array();
-foreach ($raw->row as $key => $value) {
+foreach ($raw->row as $value) {
 	// print_r($value);
-	array_push($json,$value->attributes());
+	array_push($json,$value;
 }
 // 
 
