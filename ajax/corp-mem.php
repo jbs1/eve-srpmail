775ac10916cbc72eed34de4a4ef398c5fa63701d
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-25 15:33:48 +0200

    Update corp-mem.php

--- ajax/corp-mem.php
+++ ajax/corp-mem.php
@@ -20,16 +20,17 @@ try {
     	$ids[$key]=$value['character_id'];
     }
     $split_ids=array_chunk($ids,1000);
-    $json=array();
-    foreach ($split_ids as $value){
-        $json=array_merge($json,$api_universe->postUniverseNames($value, $datasource));
-    }
-    echo'
-    <script>
-    console.log(\'json\',JSON.parse('.json_encode($json).'));
-    </script>
-    ';
-    echo json_encode($json);
+    echo $split_ids;
+//     $json=array();
+//     foreach ($split_ids as $value){
+//         $json=array_merge($json,$api_universe->postUniverseNames($value, $datasource));
+//     }
+//     echo'
+//     <script>
+//     console.log(\'json\',JSON.parse('.json_encode($json).'));
+//     </script>
+//     ';
+//     echo json_encode($json);
 } catch (Exception $e) {
     echo 'Exception: ', $e->getMessage(), PHP_EOL;
 }
