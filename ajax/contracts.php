0161336ed003dd1233de34270b0d6688881f57c5
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-04-26 03:16:55 +0200

    contract refin

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -11,8 +11,9 @@ header('Content-Type: application/json;charset=utf-8');
 $raw=getcontract(charid(),token())->result->rowset;
 $json=array();
 foreach ($raw->row as $value) {
-	echo $value["issuerID"];
-	array_push($json,$value);
+	if($value["issuerID"]==charid()&&$value["availability"]=="Private"&&$value["type"]=="ItemExchange"){
+		array_push($json,$value);
+	}
 }
 
 echo json_encode($json);
