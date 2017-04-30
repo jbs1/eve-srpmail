e8583bcf61a7022aa50a8e8de49a0df0b944b4cd
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-01 01:28:43 +0200

    fix multi append test multiple contracts

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -13,7 +13,8 @@ $json=array();
 
 //shows only the most recent privat item exchanges issued by the logged in user
 foreach ($raw->row as $value) {
-	if($value["issuerID"]==charid()&&$value["availability"]=="Private"&&$value["type"]=="ItemExchange"&&strtotime($value["dateIssued"])>strtotime('-6 hour')){
+	// &&strtotime($value["dateIssued"])>strtotime('-6 hour')
+	if($value["issuerID"]==charid()&&$value["availability"]=="Private"&&$value["type"]=="ItemExchange"){
 		array_unshift($json,$value);
 	}
 }
