33316968b3a95fd9f86dc65f7c010df1891ca1df
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-01 16:25:38 +0200

    form

--- ajax/contract-form.php
+++ ajax/contract-form.php
@@ -1,13 +1,28 @@
+<h3>forum header</h3>
+
 <form>
 <div class="form-group">
 <textarea class="form-control" id="intro-text" rows="8" readonly>
-vlalsdsa</br>
-dsa</br>
-sda</br>
-dsa</br>
-as</br>
-a</br>
+vlalsdsa<br>
+dsa<br>
+sda<br>
+dsa<br>
+as<br>
+a<br>
 </textarea>
-
 </div>
+
+<div class="form-group">
+<label for="optional-text">Optional comments for the mail</label>
+<textarea class="form-control" id="optional-text" rows="5">
+</textarea>
+<small id="optional-help" class="form-text text-muted">Use <pre><br></pre> to start a new line</small>
+</div>
+
+<div class="form-group">
+<textarea class="form-control" id="end-text" rows="8" readonly>
+vlalsdsa<br>
+</textarea>
+</div>
+
 </form>
