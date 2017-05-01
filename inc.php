ef62200bbeb30d244cf7bb9de7240b0db7c74971
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-01 17:21:14 +0200

    form

--- inc.php
+++ inc.php
@@ -44,7 +44,7 @@ function corpid($charid){
 
 function getcontract($charid,$token){
 	$url="https://api.eveonline.com/char/Contracts.xml.aspx?characterID=".$charid."&accessToken=".$token;
-	print_r($url)
+	print_r($url);
 	$ch=curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_FAILONERROR,1);
