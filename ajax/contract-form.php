e5693f3a9205c96160c84fafa6d4231d989d5c1c
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-01 16:16:28 +0200

    form

--- ajax/contract-form.php
+++ ajax/contract-form.php
@@ -1,3 +1,13 @@
-<?php
-print_r($_GET);
-?>
+<form>
+<div class="form-group">
+<textarea class="form-control" id="intro-text" rows="8" readonly>
+vlalsdsa</br>
+dsa</br>
+sda</br>
+dsa</br>
+as</br>
+a</br>
+</textarea>
+
+</div>
+</form>
