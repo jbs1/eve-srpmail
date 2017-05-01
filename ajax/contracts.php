c0624d4dfaf18d8f0f870a29216e9215cbbec2ad
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-01 15:45:14 +0200

    table cliackable

--- ajax/contracts.php
+++ ajax/contracts.php
@@ -15,7 +15,8 @@ $cid=corpid(charid());
 //shows only the most recent privat item exchanges issued by the logged in user
 foreach ($raw->row as $value) {
 	//600 ms without corpid check vs 2.5 sec with corpid check on 13 out of max 50 contracs
-	if((strtotime($value["dateIssued"])>strtotime('-6 hour'))&&($value["status"]=="Outstanding"||$value["status"]=="Completed")&&$value["issuerID"]==charid()&&$value["availability"]=="Private"&&$value["type"]=="ItemExchange"&&corpid($value["assigneeID"])==$cid){
+	//(strtotime($value["dateIssued"])>strtotime('-6 hour'))&&
+	if(($value["status"]=="Outstanding"||$value["status"]=="Completed")&&$value["issuerID"]==charid()&&$value["availability"]=="Private"&&$value["type"]=="ItemExchange"&&corpid($value["assigneeID"])==$cid){
 		array_unshift($json,$value);
 	}
 }
