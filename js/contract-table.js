ed5d76b35f51d29b97c9abc30b2b1bc6e55c7767
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-01 16:04:57 +0200

    table cliackable

--- js/contract-table.js
+++ js/contract-table.js
@@ -21,7 +21,8 @@ $(function (){
 								url: 'ajax/contract-form.php',
 								data: {"contID":item["@attributes"].contractID,"station":item["@attributes"].startStationID, "assignee":item["@attributes"].assigneeID},
 								success: function(data){
-									$('#accept').html(data);
+									$("table#cont-table").hide();
+									$('#accept').append(data);
 								}
 							})
 						})
