053c9f6be5a8aee08ac74244a017839aa1bb7707
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-02 03:07:51 +0200

    form

--- form/accept.php
+++ form/accept.php
@@ -8,7 +8,32 @@ require_once('../inc.php');
 
 token_refresh();
 
-print_r($_POST);
+// print_r($_POST);
+
+
+
+$api_instance = new Swagger\Client\Api\MailApi();
+$resp = new \Swagger\Client\Model\PostCharactersCharacterIdMailRecipient();
+$resp->recipient_type = "character";
+$resp->recipient_id = $_POST["recv"];
+$mail = new \Swagger\Client\Model\PostCharactersCharacterIdMailMail();
+$mail->subject = $_POST["subj"];
+$mail->body = $_POST["body"];
+$mail->recipients = $resp;
+$datasource = "tranquility";
+
+
+print_r($resp);
+print_r($mail);
+
+// try {
+//     $result = $api_instance->postCharactersCharacterIdMail($character_id, $mail, $datasource,);
+//     print_r($result);
+// } catch (Exception $e) {
+//     echo 'Exception when calling MailApi->postCharactersCharacterIdMail: ', $e->getMessage(), PHP_EOL;
+// }
+
+
 
 
 ?>
