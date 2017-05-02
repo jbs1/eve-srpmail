8205c58b9bee30199870eda769e1c68473b52d5c
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-02 04:24:46 +0200

    form

--- ajax/contract-form.php
+++ ajax/contract-form.php
@@ -20,17 +20,17 @@ $station=$api_universe->getUniverseStationsStationId($_GET["station"], $datasour
 <form>
 <div class="form-group">
 <label for="reciever">Reciever:</label>
-<input type="text" class="form-control" id="reciever-text" value="<?php echo $_SESSION['corpmem'][$_GET["assignee"]]; ?>" readonly>
+<input type="text" class="form-control" id="reciever-text" name="reciever-text" value="<?php echo $_SESSION['corpmem'][$_GET["assignee"]]; ?>" readonly>
 </div>
-<input type="hidden" id="reciever" value="<?php echo $_GET["assignee"]; ?>"></input>
+<input type="hidden" id="reciever" name="reciever" value="<?php echo $_GET["assignee"]; ?>"></input>
 
 <div class="form-group">
 <label for="subject">Subject:</label>
-<input type="text" class="form-control" id="subject" value="Your Ship Replacement" readonly>
+<input type="text" class="form-control" id="subject" name="subject" value="Your Ship Replacement" readonly>
 </div>
 
 <div class="form-group">
-<textarea class="form-control" id="intro-text" rows="8" readonly>
+<textarea class="form-control" id="intro-text" name="intro-text" rows="8" readonly>
 Hello,<br><br>
 I have reviewed and approved your SRP request.<br><br>
 Your replacement ship is avialable at the following station: <url=showinfo:<?php echo $station['type_id']."//".$station['station_id'].">".$station['name'];?></url><br><br>
@@ -40,13 +40,13 @@ The contract for the ship can be found here: <url=contract:<?php echo $station['
 
 <div class="form-group">
 <label for="optional-text">Optional comments for the mail</label>
-<textarea class="form-control" id="optional-text" rows="4">
+<textarea class="form-control" id="optional-text" name="optional-text" rows="4">
 </textarea>
 <small id="optional-help" class="form-text text-muted">Use &quot;&lt;br&gt;&quot; to start a new line</small>
 </div>
 
 <div class="form-group">
-<textarea class="form-control" id="end-text" rows="4" readonly>
+<textarea class="form-control" id="end-text" name="end-text" rows="4" readonly>
 <br>Fly Safe,<br>
 <url=showinfo:1377//<?php echo charid(); ?>><?php echo $_SESSION['corpmem'][charid()]; ?></url><br>
 <url=showinfo:2//917701062>EVE University</url>
