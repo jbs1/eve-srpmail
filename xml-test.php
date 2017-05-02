fa53632a59818ecd8fd59a3de0ab4ebcbb9f8334
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-02 13:58:24 +0200

    xml test

--- xml-test.php
+++ xml-test.php
@@ -6,6 +6,6 @@ token_refresh();
 $url="https://api.eveonline.com/char/Contracts.xml.aspx?characterID=".charid()."&accessToken=".token();
 print_r($url);
 
-print_r(getcontract(charid(),token())->result->rowset);
+print_r(getcontract(charid(),token()));
 
 ?>
