eae01f4dae1d848d3870681cad658c91d362ad14
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-04 14:39:54 +0200

    alert

--- index.php
+++ index.php
@@ -63,6 +63,10 @@ echo '
 		</div>
 	</div>
 	<div class="row">
+		<div class="col-md-6 col-md-offset-3" id="art">
+		</div>
+	</div>
+	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 
 			<!-- Nav tabs -->
--- js/reject-table.js
+++ js/reject-table.js
@@ -26,6 +26,10 @@ $(function (){
 											data: {'recv':$(this).find('#reciever').val(),'subj':$(this).find('#subject').val(),'body':body},
 											success: function(data){
 												console.log('Mail-ID',data);
+												if{data["success"]}{
+													var alt=$(#art).append('<div class="alert alert-success" role="alert" >Mail Sent! Mail-ID:'.data["return"].'</div>')
+													alt.alert();
+												}
 												$('div#rjctfrm').remove();
 												$("div#reject-table").show(350);
 											},
