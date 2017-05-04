a4258a8e438ee8e84f6366b5af5c7d7f701b4f88
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-05-04 17:32:46 +0200

    make mem table gen more efficient

--- js/resubmit-table.js
+++ js/resubmit-table.js
@@ -1,63 +1,51 @@
-//save members in global js variable, session caching is done at the endpoint
 $(function (){
-	// $('a[data-toggle="tab"][href="#resubmit"]').on('shown.bs.tab', function (e) {
-		if($("table#resub-table > tbody >tr").length == 0){
-			$('table#resub-table').on('update-mem', function(e){
-				// })
-			// $.ajax({
-			// 	type: 'GET',
-			// 	url: 'ajax/corp-mem.php',
-			// 	success: function(data){
-				console.log(mem)
-					$.each(mem,function(key, val){
-						var row = $("table#resub-table > tbody").append("<tr id="+key+"><td>"+val+"</td><td>"+key+"</td></tr>");
-						row.css('cursor', 'pointer');
-						row.children('#'+key).click(function () {
-							$.ajax({
-								type: 'GET',
-								url: 'ajax/resubmit-form.php',
-								data: {"assignee":key},
-								success: function(data){
-									$("div#resubmit-table").hide(350);
-									var form=$('#resubmit').append(data).find('div#rsmbfrm > form');
-									form.submit(function(e) {
-										e.preventDefault();
-										var body =$(this).find('#intro-text').val()+$(this).find('#additional-text').val()+'<br><br>'+$(this).find('#end-text').val();
-										$.ajax({
-											method: 'POST',
-											url: 'form/resubmit.php',
-											data: {'recv':$(this).find('#reciever').val(),'subj':$(this).find('#subject').val(),'body':body},
-											success: function(data){
-												if(data["success"]){
-													var alt=$('#art').append('<div class="alert alert-success alert-dismissable" role="alert" ><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> Mail Sent! Mail-ID:'+data["return"]+'</div>')
-													alt.alert();
-												}else{
-													var alt=$('#art').append('<div class="alert alert-danger" role="alert" ><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+data["return"]+'</div>')
-													alt.alert();
-												}
-												$('div#rsmbfrm').remove();
-												$("div#resubmit-table").show(350);
-											},
-										})
-									})
-								},
-								complete: function(){
-									$('#resubmit').find('div#rsmbfrm > form > button#back').click(function() {
+	if($("table#resub-table > tbody >tr").length == 0){
+		$('table#resub-table').on('update-mem', function(e){
+			$.each(mem,function(key, val){
+				var row = $("table#resub-table > tbody").append("<tr id="+key+"><td>"+val+"</td><td>"+key+"</td></tr>");
+				row.css('cursor', 'pointer');
+				row.children('#'+key).click(function () {
+					$.ajax({
+						type: 'GET',
+						url: 'ajax/resubmit-form.php',
+						data: {"assignee":key},
+						success: function(data){
+							$("div#resubmit-table").hide(350);
+							var form=$('#resubmit').append(data).find('div#rsmbfrm > form');
+							form.submit(function(e) {
+								e.preventDefault();
+								var body =$(this).find('#intro-text').val()+$(this).find('#additional-text').val()+'<br><br>'+$(this).find('#end-text').val();
+								$.ajax({
+									method: 'POST',
+									url: 'form/resubmit.php',
+									data: {'recv':$(this).find('#reciever').val(),'subj':$(this).find('#subject').val(),'body':body},
+									success: function(data){
+										if(data["success"]){
+											var alt=$('#art').append('<div class="alert alert-success alert-dismissable" role="alert" ><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> Mail Sent! Mail-ID:'+data["return"]+'</div>')
+											alt.alert();
+										}else{
+											var alt=$('#art').append('<div class="alert alert-danger" role="alert" ><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+data["return"]+'</div>')
+											alt.alert();
+										}
 										$('div#rsmbfrm').remove();
 										$("div#resubmit-table").show(350);
-									});
-								}
+									},
+								})
 							})
-						})
+						},
+						complete: function(){
+							$('#resubmit').find('div#rsmbfrm > form > button#back').click(function() {
+								$('div#rsmbfrm').remove();
+								$("div#resubmit-table").show(350);
+							});
+						}
 					})
-				// },
-				// complete: function(){
-					$('#resub-table').searchable({
-						searchField:'#resubmit-search',
-						clearOnLoad: true
-					});
-				// }
+				})
 			})
-		}
-	})
-// })
+			$('#resub-table').searchable({
+				searchField:'#resubmit-search',
+				clearOnLoad: true
+			});
+		})
+	}
+})
